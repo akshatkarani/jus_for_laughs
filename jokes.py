a96a1490d1fe0d941e5f4695421c0f06c18c4b7c
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-10-17 21:41:36 -0400

    handle jokes longer than 280 characters

--- jokes.py
+++ jokes.py
@@ -1,11 +1,26 @@
 import time
 from reddit import get_newest
-from twitter import post_tweet
+from twitter import post_tweet, post_thread
 
 
 def post():
     for tweet in get_newest():
-        post_tweet(tweet)
+        # One-tweet joke
+        if len(tweet) <= 280:
+            post_tweet(tweet)
+        # Threaded (multiple-tweet) joke
+        else:
+            tweets = []
+            tweet_words = tweet.split()
+            t = ""
+            for word in tweet_words:
+                # we check against 279 because we add a space
+                if (len(t) + len(word)) <= 279:
+                    t += word + " "
+                else:
+                    tweets.append(t)
+                    t = word
+            post_thread(tweets)
 
 
 if __name__ == '__main__':
