319de379695b18c5ec4ac9bbb6fb1c75d07866d8
akshatkarani <akshatkarani@gmail.com>
2019-10-31 19:10:23 +0530

    twitter.py: Update post_thread function
    Condition to check when a thread should be tweeted was
    wrong.

--- twitter.py
+++ twitter.py
@@ -1,6 +1,5 @@
 import os
 import tweepy
-from typing import List
 
 def _init_twitter():
     auth = tweepy.OAuthHandler(os.environ['TWEET_API_KEY'],
@@ -21,24 +20,23 @@ def post_tweet(tweet: str):
     except tweepy.error.TweepError as e:
         print(e)
 
-def post_thread(tweets: List[str]):
+def post_thread(tweets: list):
     """
     Posts a thread of tweets, received as a list
     of strings that are less than 280 characters each.
     """
-
     api = _get_api()
     try:
         last_status_id = None
         for tweet in tweets:
-            if last_status_id is not None:
+            if last_status_id is None:
                 status = api.update_status(tweet)
-                success_msg = 'Tweeted: %s (%s)'
+                success_msg = 'Succesfully Tweeted: %s (%s)'
                 info = (tweet, str(status.id))
                 print(success_msg % info)
             else:
                 status = api.update_status(tweet, last_status_id)
-                success_msg = 'Tweeted: %s (%s) as a reply to tweet %s'
+                success_msg = 'Successfully Tweeted: %s (%s) as a reply to tweet %s'
                 info = (tweet, str(status.id), str(last_status_id))
                 print(success_msg % info)
             last_status_id = status.id
